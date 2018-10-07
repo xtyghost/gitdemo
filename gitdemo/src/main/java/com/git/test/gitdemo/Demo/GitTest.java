e21b4234d67c0d308955c2fe45991f65d05612ab
xtyghost <xtyghost@163.com>
2018-10-07 19:41:33 +0800

    git的xiaodemo2

--- gitdemo/src/main/java/com/git/test/gitdemo/Demo/GitTest.java
+++ gitdemo/src/main/java/com/git/test/gitdemo/Demo/GitTest.java
@@ -26,6 +26,7 @@ public class GitTest {
         System.out.println(new Date());
         Object securityContext = System.getSecurityManager().getSecurityContext();
         System.out.println(securityContext);
+        System.out.println(1);
     }
 
 }
